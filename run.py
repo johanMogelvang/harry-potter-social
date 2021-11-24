6999eed2c8d2d361dd6191200650383693be3c3c
texbomb <johanmogelvang@gmail.com>
2021-11-24 11:51:22 +0100

    tryout

---
+++ run.py
@@ -0,0 +1,39 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from flask import Flask
+import os
+
+server = Flask(__name__)
+server.secret_key = os.environ.get('secret_key', 'secret')
+app = dash.Dash(name = __name__, server = server)
+app.config.supress_callback_exceptions = True
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+    dcc.Graph(
+        id='example-graph',    
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    ),
+    dcc.Input(id='my-id', value='initial value', type="text"),
+    html.Div(id='my-div')
+])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
