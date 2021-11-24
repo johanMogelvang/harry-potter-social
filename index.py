5ced31e8d84ea15be8e0f136cb927d834e5462e7
texbomb <johanmogelvang@gmail.com>
2021-11-24 16:01:08 +0100

    Run from index now, due to multi page setup

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
-from app import app
+from app import app, server
 # import all pages in the app
 from apps import family_house, home, exploratory
 
