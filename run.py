c6f09c1f8027916e6507a01dd0e5a6cfdbe71585
texbomb <johanmogelvang@gmail.com>
2021-11-24 12:02:58 +0100

    test 2

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import dash
+from dash import html
 import dash_core_components as dcc
-import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from flask import Flask
 import os
